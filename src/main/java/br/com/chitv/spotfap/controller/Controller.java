7dc561edb267a3e600355dd5f50386230e33b312
Ismael Barbosa <ibf321@gmail.com>
2016-10-19 19:07:04 -0300

    JSF + PrimeFaces webapp

--- src/main/java/br/com/chitv/spotfap/controller/Controller.java
+++ src/main/java/br/com/chitv/spotfap/controller/Controller.java
@@ -1,5 +1,24 @@
 package br.com.chitv.spotfap.controller;
 
-public class Controller {
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+@Named
+@RequestScoped
+public class Controller implements Serializable{
+	
+	private static final long serialVersionUID = -8391472676388527662L;
+
+	@Transactional
+	public String irPagina(String link) {
+		return "/" + link + ".xhtml";
+	}
+	
 }
