744f817054c2c09e8b65653f3888932bc5eaa768
Ismael Barbosa <ibf321@gmail.com>
2016-11-23 18:43:40 -0300

    Usuário

--- src/main/java/br/com/chitv/sportfap/dao/UsuarioDao.java
+++ src/main/java/br/com/chitv/sportfap/dao/UsuarioDao.java
@@ -1,10 +1,7 @@
 package br.com.chitv.sportfap.dao;
 
-
-
-
-
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.enterprise.context.Dependent;
@@ -14,29 +11,28 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 
+import org.apache.http.impl.io.SocketOutputBuffer;
 
 import br.com.chitv.sportfap.model.Usuario;
 import br.com.chitv.sportfap.model.UsuarioProfile;
-
 
 @Named
 @Dependent
 public class UsuarioDao {
 
-	@PersistenceContext(unitName="sportfap")
+	@PersistenceContext(unitName = "sportfap")
 	private EntityManager emM;
 
 	@Transactional
 	public void excluir(Usuario usuario) {
-		this.emM.remove(this.emM.contains(usuario) ? usuario : this.emM.merge(usuario) );
+		this.emM.remove(this.emM.contains(usuario) ? usuario : this.emM.merge(usuario));
 	}
 
-	
 	public List<Usuario> listaUsuarios() {
 		TypedQuery<Usuario> query = emM.createQuery("select u from Usuario u", Usuario.class);
 		return query.getResultList();
 	}
-	
+
 	@Transactional
 	public void salvar(Usuario usuario) {
 		this.emM.merge(usuario);
@@ -53,38 +49,47 @@ public class UsuarioDao {
 		List<Usuario> usuarios = query.getResultList();
 		return usuarios.isEmpty() ? null : usuarios.get(0);
 	}
-	
-	public Usuario findByNomsenha(String nome, String senha) {
-		String queryStr = "select u from Usuario u where u.nome = :nome and u.senha = :senha";
-		TypedQuery<Usuario> query = this.emM.createQuery(queryStr, Usuario.class);
-		query.setParameter("nome", nome);
-		query.setParameter("senha", senha);
-		List<Usuario> usuarios = query.getResultList();
-		return usuarios.isEmpty() ? null : usuarios.get(0);
+
+	public boolean findByNomeSenha(String nome, String senha) {
+		// String queryStr = "select u from Usuario u where u.nome = :nome and
+		// u.senha = :senha";
+		// TypedQuery<Usuario> query = this.emM.createQuery(queryStr,
+		// Usuario.class);
+		// query.setParameter("nome", nome);
+		// query.setParameter("senha", senha);
+		// List<Usuario> usuarios = query.getResultList();
+		// return usuarios.isEmpty() ? null : usuarios.get(0);
+
+		if (findByNome(nome) != null) {
+			Usuario userL = findByNome(nome);
+
+			if (userL.getNome().equals(nome) && userL.getSenha().equals(senha)) {
+				return true;
+			}
+			
+		}
+		return false;
+
 	}
+
 	public Usuario findByNomeFake(String nome) {
 		Usuario usuario = new Usuario();
 		usuario.setNome("ismael");
 		usuario.setId(1L);
-		
-		
+
 		List<Usuario> usuarios = new ArrayList<Usuario>();
 		usuarios.add(usuario);
-		
+
 		UsuarioProfile usuarioProfile = new UsuarioProfile();
 		usuarioProfile.setId(1L);
 		usuarioProfile.setType("ROLE_ADMIN");
 		usuarioProfile.setUsuarios(usuarios);
-		
+
 		List<UsuarioProfile> usuarioProfiles = new ArrayList<UsuarioProfile>();
 		usuarioProfiles.add(usuarioProfile);
-		
-	
-		
-		return usuario; 
+
+		return usuario;
 	}
-
-
 
 	public Usuario getUsuario(String nome, String senha) {
 		// TODO Auto-generated method stub
@@ -92,7 +97,3 @@ public class UsuarioDao {
 	}
 
 }
-
-
-
-
